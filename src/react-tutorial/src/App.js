43bb5ce1555ab3d42025821b399d9f11a25b63e2
豊田 <toyoda@toyodanoMacBook-puro-2.local>
2020-05-02 19:05:29 +0900

    second commit

--- src/react-tutorial/src/App.js
+++ src/react-tutorial/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import propTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HelloWorld!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const profiles=[
+        {name: "taro", age: 10},
+        {name: "hanako", age: 10},
+        {name: "moe"}
+    ]
+    return (
+        <div>
+            {
+                profiles.map((profile, index) => {
+                    return <User name={profile.name} age={profile.age} key={index}/>
+                })
+            }
+            <Counter></Counter>
+        </div>
+    )
 }
 
+const User = (props) => {
+    return <div>Meow! {props.name}, age = {props.age}</div>
+}
+
+class Counter extends Component {
+    constructor(props){
+        super(props)
+        this.state = { count: 0}
+    }
+    handlePlusButton = () => {
+        const count = this.state.count + 1
+        this.setState(
+            {count: count}
+        )
+    }
+    render(){
+        return(
+            <div>
+                <div>counter = {this.state.count}</div>
+                <button onClick={this.handlePlusButton}>+1</button>
+                <button>-1</button>
+            </div>
+        )
+    }
+}
+User.defaultProps = {
+    age: 1
+}
+User.propTypes = {
+    name: propTypes.string
+}
 export default App;
